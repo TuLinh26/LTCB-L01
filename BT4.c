0aaa32ff538caf6a057892ffe437f68a968c152b
TuLinh26 <khanhlinhtuthi@gmail.com>
2025-03-04 01:45:30 +0700

    BT4.c

--- BT4.c
+++ BT4.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 int main(){
-    int R,S,V;
-    scanf ("Nhap ban kinh hinh cau: %f",&R);
-    printf ("The tich hinh cau: %f",V=(4.0/3)*3.14*R*R*R);
-    return 0;
+    float R;
+    const float pi=3.1415;
+    float S, V;
+    S=4 * pi * R * R;
+    V=(4.0 / 3) * pi * R * R * R;
+    scanf("nhập bán kính %.2f", &R);
+    printf("Dien tich cua hinh cau la: %.2f\n", S);
+    printf("The tich cua hinh cau la: %.2f\n", V);
 }
