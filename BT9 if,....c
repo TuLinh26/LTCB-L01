1b7c9b0483e0839567c8d3431d4a62e517d6ef27
TuLinh26 <khanhlinhtuthi@gmail.com>
2025-04-16 22:52:55 +0700

    Update and rename BT9.c to BT9 if,....c

--- BT9 if,....c
+++ BT9 if,....c
@@ -8,9 +8,9 @@ int main() {
     if (m<1||m>12)
         printf("Khong ton tai\n");
     else {
-        if (m==2)(d=((y%4==0&&y%100!=0)||y%400==0))? 29 : 28;
-        else if (m==4||m==6||m==9||m==11) d=30;
-        else d=31;
+        if (m == 2)(d =((y % 4 == 0 && y % 100 != 0)||y % 400 == 0))? 29 : 28;
+        else if (m == 4|| m == 6|| m == 9|| m == 11) d = 30;
+        else d = 31;
         printf("Thang %d nam %d co %d ngay\n",m,y,d);
     }
     return 0;
