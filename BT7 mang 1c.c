8ff25ee7956a022819d5739220362f91af70e47a
TuLinh26 <khanhlinhtuthi@gmail.com>
2025-04-16 23:33:19 +0700

    BT7 mang 1c.c

--- BT7 mang 1c.c
+++ BT7 mang 1c.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 #include <math.h>
-void swap(int *l, int *h){
-    int tmp=*l;
-    *l=*h;
-    *h=tmp;
-}
-void sort(int a[],int n){
-    for (int i=0; i<n-1;i++){
-        for (int j=i+1;j<n;j++){
-            if (a[i]>a[j]){
-                swap(&a[i],&a[j]);
-            }
-        }
-    }
+int comp(int *a, int *b){
+    int *x = (int*)a;
+    int *y = (int*)b;
+    return *x - *y;
 }
 int main(){
     int n;
-    scanf ("%d",&n);
+    printf ("Nhap phan tu cua mang: ");
+    scanf ("%d", &n);
     int a[n];
+    printf ("Nhap mang: ");
     for (int i = 0; i < n; i++){
         scanf ("%d", &a[i]);
     }
-    int x=10;
-    int y=20;
-    sort(a,n);
+    qsort (a, n, sizeof(int),comp);
     for (int i = 0; i < n; i++){
-        printf ("%d ",a[i]);
+        printf ("%d ", a[i]);
     }
 }
 
